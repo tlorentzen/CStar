49986f22df739a79d34ed29dd154952e40a61c81
Idadorable <44022898+Idadorable@users.noreply.github.com>
2020-03-05 15:48:17 +0100

    Add files via upload

--- Eksempel_lyskryds_cstar.ino
+++ Eksempel_lyskryds_cstar.ino
@@ -1,15 +1,15 @@
 
 unchangeable pin button = A0;
 
-unchangeable pin array topLed = {8, -1, -1};
-unchangeable pin array middleLed = {11, 10, -1};
-unchangeable pin array bottomLed = {-1, 4, -1};
+pin array topLed = [8, -1, -1];
+unchangeable pin array middleLed = [11, 10, -1];
+unchangeable pin array bottomLed = [-1, 4, -1];
 
 
-color red = {255, 0, 0};
-color green = {0, 255, 0};
-color yellow = {255, 70, 0};
-color noColor = {0, 0, 0};
+color red = [255, 0, 0];
+color green = [0, 255, 0];
+color yellow = [255, 70, 0];
+color noColor = [0, 0, 0];
 
 big integer blinkSpeed = 100;
 integer blinkTime = 10;
@@ -30,35 +30,35 @@ void loop() {
 
 void changeLight(){
   if(colorState IS 0){
-    write(topLed, noColor);
-    write(middleLed, yellow);
+    topLed = noColor);
+    middleLed = yellow);
     colorState = 1;
   }
-  else if (colorState IS 1){
-    write(middleLed, noColor);
-    write(bottomLed, green);
+  if (colorState IS 1){
+    middleLed = noColor);
+    bottomLed = green);
     greenBlink();
     colorState = 2;
     }
   else{
-    write(bottomLed, noColor);
-    write(topLed, red);
+    bottomLed = noColor);
+    topLed = red);
     colorState = 0;
   }
 }
 
 
 void greenBlink(){
-  positive integer buttonState = read(button);
+  positive integer buttonState = button;
   console.print(buttonState);
   start = true;
   sleep(300);
   
   while(buttonState IS 0 OR start IS true) repeat{
-    buttonState = read(button);
-    write(bottomLed, noColor);
+    buttonState = button;
+    bottomLed = [0,0,0];
     sleep(blinkSpeed);
-    write(bottomLed, green);
+    bottomLed = green);
     sleep(blinkSpeed);
     start = false;
   }
