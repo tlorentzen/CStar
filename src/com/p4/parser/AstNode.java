b37ad9019b4eea034fdbca57c7e1f88c8a0b3a52
JakobLund <jlund18@student.aau.dk>
2020-04-08 11:48:13 +0200

    Deleted abstact from astnode

--- src/com/p4/parser/AstNode.java
+++ src/com/p4/parser/AstNode.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.*;
 
-public abstract class AstNode {
+public class AstNode {
     Token token; // node is derived from which token?
     List<AstNode> children; // operands
     public AstNode(Token token) { this.token = token; }
