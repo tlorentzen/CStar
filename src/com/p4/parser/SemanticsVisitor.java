57c144839bd79aee07ac42656a5421dcd9556e56
JakobLund <jlund18@student.aau.dk>
2020-04-23 14:53:52 +0200

    A commit

--- src/com/p4/parser/SemanticsVisitor.java
+++ src/com/p4/parser/SemanticsVisitor.java
@@ -27,15 +27,19 @@ public class SemanticsVisitor implements INodeVisitor {
     public void visit(IdNode node){
         if(!this.symbolTable.declaredInAccessibleScope(node.id)){
             errors.addEntry(ErrorType.TYPE_ERROR, node.id + " has not been declared in any accessible scope. The type of " + node.id + " will be null", node.lineNumber);
-            //Todo: set the node.type to something to avoid null pointer exception½
+            //Todo: set the node.type to something to avoid null pointer exception
         } else {
             node.type = symbolTable.lookup(node.id).variableType;
         }
     }
 
-    public void visit(IntegerNode node){ node.type = "int"; }
+    public void visit(IntegerNode node){
+        node.type = "integer";
+    }
 
-    public void visit(FloatNode node){ node.type = "decimal"; }
+    public void visit(FloatNode node){
+        node.type = "decimal";
+    }
 
     public void visit(PinNode node){
         node.type = "pin";
