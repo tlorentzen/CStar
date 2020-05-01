bcfe594b6988e1e4bb4a46ac150d401cc0d81100
Thomas Lorentzen <thomas@tlorentzen.net>
2020-05-01 11:26:35 +0200

    Minor

--- src/com/p4/Main.java
+++ src/com/p4/Main.java
@@ -89,7 +89,7 @@ public class Main {
                                     //Todo: should we do something
                                 }
                             }else{
-                                errors.addEntry(ErrorType.MISSING_FUNCTION, "Both the functions 'setup()' and 'loop()' are required");
+                                errors.addEntry(ErrorType.MISSING_FUNCTION, "Both the functions 'void setup()' and 'void loop()' are required");
                             }
                         }
                     }catch (IOException e){
