50589f1191f13f6d72d2f34bdb95feb758bc18b9
Thomas Lorentzen <thomas@tlorentzen.net>
2020-05-04 11:03:11 +0200

    Comment

--- src/com/p4/parser/visitors/AstVisitor.java
+++ src/com/p4/parser/visitors/AstVisitor.java
@@ -7,6 +7,8 @@ import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.TerminalNodeImpl;
 
+import javax.xml.stream.events.Comment;
+
 public class AstVisitor<T> extends CStarBaseVisitor<AstNode> {
 
     @Override
@@ -197,6 +199,12 @@ public class AstVisitor<T> extends CStarBaseVisitor<AstNode> {
         } else {
             return null;
         }
+    }
+
+    @Override
+    public AstNode visitComment(CStarParser.CommentContext ctx) {
+        CommentNode node = new CommentNode("Hej");
+        return node;
     }
 
     @Override
