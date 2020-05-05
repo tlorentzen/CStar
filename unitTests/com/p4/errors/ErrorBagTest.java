1799b8f036629beb89a2a6a5e1122af3316e1e14
Niels Vistisen <nielswps@gmail.com>
2020-05-05 08:38:05 +0200

    ErrorBag test class created

---
+++ unitTests/com/p4/errors/ErrorBagTest.java
@@ -0,0 +1,36 @@
+package com.p4.errors;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ErrorBagTest {
+
+    @Test
+    void addEntry() {
+    }
+
+    @Test
+    void testAddEntry() {
+    }
+
+    @Test
+    void testAddEntry1() {
+    }
+
+    @Test
+    void testAddEntry2() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void display() {
+    }
+
+    @Test
+    void containsErrors() {
+    }
+}
