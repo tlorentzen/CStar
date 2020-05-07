fb31433fdd93f5881f65703b14036884a43a532b
Niels Vistisen <nielswps@gmail.com>
2020-05-07 13:55:07 +0200

    Unit tests

--- unitTests/com/p4/parser/visitors/AstVisitorTest.java
+++ unitTests/com/p4/parser/visitors/AstVisitorTest.java
@@ -1,21 +1,30 @@
 package com.p4.parser.visitors;
 
+import com.p4.syntaxSemantic.visitors.AstVisitor;
+
 class AstVisitorTest {
 
-    /*@Test
-    void visitProgWithProgContextWithEmptyContext(){
-        AstVisitor astVisitor = new AstVisitor();
-        CStarParser.ProgContext progContext = new CStarParser.ProgContext(new ParserRuleContext(),0);
-        AstNode astNode = astVisitor.visitProg(progContext);
-        assertEquals(0,astNode.children.size());
+    private AstVisitor astVisitor = new AstVisitor();
 
+    /*@Test
+    void visitVal_ReceivesContextOfNegativeNumber_ReturnsCorrectValNode(){
+        //Arrange
+        var valContext = new CStarParser.ValContext(new ParserRuleContext(), 0);
+        valContext.NUMBER() = (CStarParser.NUMBER);
+
+        //Act
+        var astNode = astVisitor.visitProg(progContext);
+        var result = astNode.children.get(0);
+
+        //Assert
+        assert(result instanceof CommentNode);
     }
 
    @Test
     void visitProgWithProgContextWithChildren(){
         AstVisitor astVisitor = new AstVisitor();
         CStarParser.ProgContext progContext = new CStarParser.ProgContext(new ParserRuleContext(),0);
-       progContext.addChild(new CStarParser.AssignContext(new ParserRuleContext(),0));
+        progContext.addChild(new CStarParser.AssignContext(new ParserRuleContext(),0));
         AstNode astNode = astVisitor.visitProg(progContext);
         assertEquals(0,astNode.children.size());
 
