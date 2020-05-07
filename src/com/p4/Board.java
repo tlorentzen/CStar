964ac5851a455bf00e604448bbc8371082f0c773
Thomas Lorentzen <thomas@tlorentzen.net>
2020-05-07 11:05:57 +0200

    Added board chnges

--- src/com/p4/Board.java
+++ src/com/p4/Board.java
@@ -5,9 +5,4 @@ public class Board {
     public String core = "";
     public String port = "";
     public String fqbn = "";
-
-    public void printInfo(){
-        System.out.println("Board: "+this.name+" - "+this.port);
-        System.out.println();
-    }
 }
