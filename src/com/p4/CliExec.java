598a9bd4c113dd4fb1413d3ad2d62d658c52755a
JakobLund <jlund18@student.aau.dk>
2020-05-07 08:53:50 +0200

    little change

--- src/com/p4/CliExec.java
+++ src/com/p4/CliExec.java
@@ -201,7 +201,7 @@ public class CliExec {
             if(execute("core update-index", acli)){
                 printOk();
             }else{
-                printFailed();;
+                printFailed();
             }
         }
     }
