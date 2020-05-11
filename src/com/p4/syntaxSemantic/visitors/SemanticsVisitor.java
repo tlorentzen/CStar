a3f8c440a8cf59f69d8cc95b02eca65045d9225b
JakobLund <jlund18@student.aau.dk>
2020-05-11 14:02:24 +0200

    Fixed pin array

--- src/com/p4/syntaxSemantic/visitors/SemanticsVisitor.java
+++ src/com/p4/syntaxSemantic/visitors/SemanticsVisitor.java
@@ -615,7 +615,7 @@ public class SemanticsVisitor implements INodeVisitor {
             //Enters if the function includes a dot operator
             if (functionName.contains(".")) {
                 String idName = functionName.split("\\.")[0];
-
+                idName = idName.substring(0, idName.length() - 3);
                 //Enters if the pin has not been declared
                 if (symbolTable.lookupSymbol(idName) == null) {
                     errors.addEntry(ErrorType.TYPE_ERROR, errorMessage("no id dcl",idName), node.lineNumber);
