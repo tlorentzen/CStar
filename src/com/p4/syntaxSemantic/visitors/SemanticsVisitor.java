6236e3db305138daf0a1397b4657d9ddc6a89ab9
JakobLund <jlund18@student.aau.dk>
2020-05-13 11:00:10 +0200

    deleted dangling else

--- src/com/p4/syntaxSemantic/visitors/SemanticsVisitor.java
+++ src/com/p4/syntaxSemantic/visitors/SemanticsVisitor.java
@@ -681,9 +681,6 @@ public class SemanticsVisitor implements INodeVisitor {
             } else {
                 checkFunction(node, functionName);
             }
-            else {
-                checkFunction(node, functionName);
-            }
 
         }
         //Enters if the function has not been declared
